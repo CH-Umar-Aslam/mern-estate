322dde0ab5cc4b1976ee4aa24ebf6ba174676ebd
CH Umar Aslam <chumaraslam22@gmail.com>
2024-05-03 16:38:57 +0500

    install react , tailwind css , and react router dom template

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './paegs/Home'
+import { About } from './paegs/About'
+import SignIn from './paegs/SignIn'
+import SignUp from './paegs/SignUp'
+import Profile from './paegs/Profile'
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+
+        < Route path='/' element={<Home />} />
+        < Route path='/about' element={<About />} />
+        < Route path='/sign-in' element={<SignIn />} />
+        < Route path='/sign-up' element={<SignUp />} />
+        < Route path='/profile' element={<Profile />} />
+      </Routes>
+
+    </BrowserRouter>
+  )
+}
+
+export default App
